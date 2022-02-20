d81ca03403d422e4d9ae8170b4e74fffbd725b5b
Viney <vineykhaneja999@gmail.com>
2022-02-20 08:37:46 +0530

    [Viney] FEATURE ADDED: Creating Contacts & Storing in list

---
+++ Contacts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    public class Contacts
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public double phoneNo { get; set; }
+        public string eMail { get; set; }
+
+        public void AddContact()
+        {
+            Console.Write("Enter first name: ");
+            this.firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            this.lastName = Console.ReadLine();
+            Console.Write("Enter address details: ");
+            this.address = Console.ReadLine();
+            Console.Write("Enter city name: ");
+            this.city = Console.ReadLine();
+            Console.Write("Enter state name: ");
+            this.state = Console.ReadLine();
+            Console.Write("Enter zip Code: ");
+            this.zip = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter phone Number: ");
+            this.phoneNo = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ebter your mail Id: ");
+            this.eMail = Console.ReadLine();
+            Console.WriteLine($"Details of{this.firstName} added successfully");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Address_Book;
 
-namespace AddressBook
-{
-    internal class Address
+namespace OOPS
+{ 
+    internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address Book Program");
+            //Storing 2 Persons details in the list by Console
+            Contacts p1 = new Contacts();
+            p1.AddContact();
+            Contacts p2 = new Contacts();
+            p2.AddContact();
+            List<Contacts> listOfContacts = new List<Contacts>();
+            listOfContacts.Add(p1);
+            listOfContacts.Add(p2);
         }
     }
 }
