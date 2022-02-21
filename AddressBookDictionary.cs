b4ab11fa2d463377ddf5335602e3deb97ae74b3d
Viney <vineykhaneja999@gmail.com>
2022-02-21 16:26:26 +0530

    [Viney] FEATURE ADDED: Multiple Address Books Created

---
+++ AddressBookDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Address;
+using ContactsDetails;
+
+namespace AddressBook
+{
+    internal class AddressBookDictionary
+    {
+        Dictionary<string,Address_Book> _multipleAddressBook = new Dictionary<string,Address_Book>();
+
+        public void AddAddressBook(string nameOfAddressBook,Address_Book address_Book)
+        {
+            _multipleAddressBook.Add(nameOfAddressBook,address_Book);
+        }
+        public void AddMultipleAdressBook()
+        {
+            Console.WriteLine("How manny address books u want?");
+            int countAddressBook = Convert.ToInt32(Console.ReadLine());
+            while(countAddressBook > 0)
+            {
+                string choiceToAddContacts = string.Empty;
+                string nameofEditOrDeleteContact = string.Empty;
+                string choiceToEditOrDelete = string.Empty;
+                string nameOfAddressBook = string.Empty;
+                Address_Book book = new Address_Book();
+                //For Adding Contact Details
+                do
+                {
+                    Contacts c = Contacts.AddContactFromConsole();
+                    book.AddContacts(c);
+                    Console.WriteLine("Do u want to add Contacts: - Y or N");
+                    choiceToAddContacts = Console.ReadLine().ToUpper();
+                } while (choiceToAddContacts != "N");
+                Console.Write("Enter the name of Address Book: ");
+                nameOfAddressBook = Console.ReadLine();
+                AddAddressBook(nameOfAddressBook, book);
+                countAddressBook--;
+            }
+        }
+        public void GetAddressBook()
+        {
+            foreach(var pair in _multipleAddressBook)
+            {
+                Console.WriteLine($"Name of Address Book is {pair.Key} & details are: ");
+                pair.Value.getContacts();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ContactsDetails;
 using Address;
+using AddressBook;
 
 namespace OOPS
 { 
@@ -12,40 +13,10 @@ namespace OOPS
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address Book Program");
-            //VARIABLES
-            string choiceToAddContacts = string.Empty;
-            string nameofEditOrDeleteContact = string.Empty;
-            string choiceToEditOrDelete = string.Empty;
-
-            Address_Book book = new Address_Book();
-            //For Adding Contact Details
-            do
-            {
-                Contacts c =  Contacts.AddContactFromConsole();
-                book.AddContacts(c);
-                Console.WriteLine("Do u want to add Contacts: - Y or N");
-                choiceToAddContacts = Console.ReadLine().ToUpper();
-            } while(choiceToAddContacts != "N");
-
-            //For Editing or Deleting Contacts
-            Console.WriteLine("Please enter name of the person for whom u want to edit or delete details: ");
-            nameofEditOrDeleteContact = Console.ReadLine();
-            Console.WriteLine("Please select E or D to edit or delete the contacts respectively on list ");
-            choiceToEditOrDelete = Console.ReadLine().ToUpper();
-            switch (choiceToEditOrDelete)
-            {
-                case "E":
-                    book.editContacts(nameofEditOrDeleteContact);
-                    break;
-                case "D":
-                    book.deleteContacts(nameofEditOrDeleteContact);
-                    break;
-                default:
-                    Console.WriteLine("Invalid Selection ! Enter E or D");
-                    break;
-            }
-            //Verifying Details are edited or deleted.
-            book.getContacts();
+            AddressBookDictionary addressBookDictionary = new AddressBookDictionary();
+            addressBookDictionary.AddMultipleAdressBook();
+            addressBookDictionary.GetAddressBook();
+            
         }
     }
 }
